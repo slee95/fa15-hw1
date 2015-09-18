eeeb2d3716cea8192289ffb8a336604cec30c05d
Soohee Lee <slee95@berkeley.edu>
2015-09-18 08:14:45 +0000

    hw1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,10 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(a)
+  	a = a.map{|i| i.to_i+2}
+  	a = a.select{|i| i % 2 == 0}
+  	a = a.uniq
+  	a = a.reject{|i| i>10}
+  	a.reduce(:+)
+  end
 end
